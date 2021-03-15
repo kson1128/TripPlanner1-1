c3e654e8cf15e22f8d5ed90c041969ab8e7c15ec
Kathy Son <kson1128@gmail.com>
2021-03-15 13:07:35 -0400

    finished markers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoia2NhcnJpbGwiLCJhIjoiY2ttYXM2MDAyMXVyMTJ2b2pld
 
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  center: [-74.009151, 40.705086], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
